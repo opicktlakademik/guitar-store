49d7dd512d0db1c2821f6becffcf2a5d8a81a382
opicktlakademik <anieffauzan.rozi@gmail.com>
2020-01-06 19:47:35 +0700

    F

--- application/views/dashboard_v.php
+++ application/views/dashboard_v.php
@@ -11,6 +11,44 @@
              </div>
              <div class="card-body">
                  <!-- TAMBAH INFORMASI TOKO DI SINI -->
+                 <div class="row">
+                     <div class="col-md-4">
+                         <div class="card">
+                             <div class="card-body ml-auto mr-auto">
+                                 <img style="width: max-content" src="<?php echo base_url() ?>assets/img/logo.png" alt="">
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col-md-8">
+                         <table class="table table-striped">
+                             <tr>
+                                 <th>Nama Toko</th>
+                                 <td width="1%">:</th>
+                                 <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. </td>
+                             </tr>
+                             <tr>
+                                 <th>Alamat Toko</th>
+                                 <td width="1%">:</th>
+                                 <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. </td>
+                             </tr>
+                             <tr>
+                                 <th>Pemilik Toko</th>
+                                 <td width="1%">:</th>
+                                 <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. </td>
+                             </tr>
+                             <tr>
+                                 <th>Instagram Toko</th>
+                                 <td width="1%">:</th>
+                                 <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. </td>
+                             </tr>
+                             <tr>
+                                 <th>Facebook Toko</th>
+                                 <td width="1%">:</td>
+                                 <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. </td>
+                             </tr>
+                         </table>
+                     </div>
+                 </div>
              </div>
              <!-- /.card-body-->
          </div>
